e78350d105f1965656fb6ab7993c9737f250d7c1
c9097666 <c9097666@CACLC9097666.red.ecopetrol.com.co>
2020-05-05 12:11:31 -0500

    Commit Initial

---
+++ facturas-1/src/main/java/com/facturas/demo/Facturas1Application.java
@@ -0,0 +1,13 @@
+package com.facturas.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Facturas1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Facturas1Application.class, args);
+	}
+
+}
---
+++ facturas-1/src/test/java/com/facturas/demo/Facturas1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facturas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Facturas1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
